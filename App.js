48cb8ccebc5289f73d0da03b7fdd01ca8436e0b1
shipstreet <orhaybenaim@gmail.com>
2018-05-23 20:47:59 +0300

    added react nativation

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container } from 'native-base';
+import { StyleSheet } from 'react-native';
+import Login from './Components/Login';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text></Text>
-      </View>
+      <Container >
+        <Login/>
+      </Container>
     );
   }
 }
