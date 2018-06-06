a9c19dded741411cfb5dd3e5e8f50b2514573621
etty tamar <etty.ohayon@gmail.com>
2018-06-06 18:51:01 +0300

    fixed register at web service

--- WebService/WebSite3/App_Code/SQL.cs
+++ WebService/WebSite3/App_Code/SQL.cs
@@ -64,45 +64,25 @@ public static class SQL
 
         User.Add("Items", Items);
 
-<<<<<<< HEAD
-        return new JavaScriptSerializer().Serialize(User);
-    }
-
-
-    static public string Register(string email, string fname, string lname, string password)
-=======
             return new JavaScriptSerializer().Serialize(User);
         }
 
     static public string Register(string email, string password , string name, string last_name)
->>>>>>> c1738d5b8012a7457000e8a9be5c801b4fb83cd0
     {
         SqlConnection con = new SqlConnection(connectionStr);
         adtr = new SqlDataAdapter($"Register", con);
         adtr.SelectCommand.CommandType = CommandType.StoredProcedure;
         adtr.SelectCommand.Parameters.Add(new SqlParameter("Email", email));
-<<<<<<< HEAD
-        adtr.SelectCommand.Parameters.Add(new SqlParameter("Fname", Fname));
-        adtr.SelectCommand.Parameters.Add(new SqlParameter("Lname", Lname));
-        adtr.SelectCommand.Parameters.Add(new SqlParameter("Pass", password));
-=======
         adtr.SelectCommand.Parameters.Add(new SqlParameter("Fname", name));
         adtr.SelectCommand.Parameters.Add(new SqlParameter("Lname", last_name));
         adtr.SelectCommand.Parameters.Add(new SqlParameter("pass", password));
->>>>>>> c1738d5b8012a7457000e8a9be5c801b4fb83cd0
 
         DataSet errorsSet = new DataSet();
         adtr.Fill(errorsSet, "Errors");
 
-<<<<<<< HEAD
-        if (DefaultSettingValueAttribute.Tables["Errors"].Rows.Count == 0)
-            return Login(email, password);
-        return ds.Tables["User_Items"].Rows[0][0].ToString();
-=======
-        if (ds.Tables["Errors"].Rows.Count == 0)
+        if (errorsSet.Tables["Errors"]  == null)
             return Login(email , password);
-        return ds.Tables["User_Items"].Rows[0][0].ToString();
+        return errorsSet.Tables["Errors"].Rows[0][0].ToString();
     }
->>>>>>> c1738d5b8012a7457000e8a9be5c801b4fb83cd0
-    }
+    
 }
--- WebService/WebSite3/App_Code/WebService.cs
+++ WebService/WebSite3/App_Code/WebService.cs
@@ -29,38 +29,13 @@ public class WebService : System.Web.Services.WebService
         return SQL.Login(email, password);
     }
 
-    using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Script.Services;
-using System.Web.Services;
-
-
-/// <summary>
-/// Summary description for WebService
-/// </summary>
-[WebService(Namespace = "http://tempuri.org/")]
-[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-[System.Web.Script.Services.ScriptService]
-public class WebService : System.Web.Services.WebService
-{
-
-    public WebService()
-    {
-
-        //Uncomment the following line if using designed components 
-        //InitializeComponent(); 
-    }
 
     [WebMethod]
-    public string register(string email, string fname, string lname, string password)
+    public string Register(string email, string fname, string lname, string password)
     {
-        return SQL.Login(email, fname, lname, password);
+        return SQL.Register(email, password, fname, lname);
     }
 
 }
 
 
-}
